74d5c1784166ed6144ae5ae5bb06df05a6dc169f
simon <simon.m.binyamin@gmail.com>
2021-12-14 17:34:10 +0100

    fix: cd

--- Program.cs
+++ Program.cs
@@ -132,10 +132,8 @@ namespace realclicore
                         if (slnProj == 0 && slnData == 0 && slnDomain == 0 && slnBusiness == 0)
                         {
 
-                            var e = await RunCli("cd ..");
-                            if(e==0) {
-                                await RunCli("rm -r realcorecli/*");
-                            }
+                            await RunCli("rm -r ../realcorecli/*");
+                            await RunCli("cd ..");
                             
                         
                         }
